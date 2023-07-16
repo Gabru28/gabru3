ce9271a11c0dec15701545ee36531e85b63f1d73
Gabru28 <139681421+Gabru28@users.noreply.github.com>
2023-07-16 16:59:10 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,12 @@
+function google(){
+    window.open('https://www.google.com/')
+}
+function youtube(){
+    window.open('https://www.youtube.com/')
+}
+function maps(){
+    window.open('https://www.google.com/maps')
+}
+function create(){
+    window.open('https://accounts.google.com/v3/signin/identifier?dsh=S-318540194%3A1689506443501171&continue=https%3A%2F%2Fwww.google.com%3Fhl%3Den-US&ec=GAlA8wE&hl=en&flowName=GlifWebSignIn&flowEntry=AddSession')
+}
